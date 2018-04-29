7fe62e82da0591904ae3059a378fac47098ed406
林房坤 <1783956729@qq.com>
2018-04-29 08:14:09 +0800

    1）添加挑战猜歌王6.0项目到GitHub。

---
+++ wxapp/siteinfo.js
@@ -0,0 +1,11 @@
+var siteinfo = {
+    name: "挑战猜歌",
+    uniacid: "4",
+    acid: "4",
+    multiid: "4",
+    version: "1.0.1",
+    siteroot: "https://www.fkunlam.com/app/index.php",
+    design_method: "3"
+};
+
+module.exports = siteinfo;
